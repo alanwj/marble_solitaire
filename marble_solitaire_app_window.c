a894974f3c9aa159008f2e4b8267bf00427bd43a
Alan Johnson <alan.wayne.johnson@gmail.com>
2015-03-22 15:07:44 -0700

    Add a new game button.

--- marble_solitaire_app_window.c
+++ marble_solitaire_app_window.c
@@ -6,6 +6,8 @@
 
 struct _MarbleSolitaireAppWindow {
   GtkApplicationWindow parent;
+  GtkWidget *grid;
+  GtkWidget *marble_grid;
 };
 
 struct _MarbleSolitaireAppWindowClass {
@@ -14,13 +16,38 @@ struct _MarbleSolitaireAppWindowClass {
 
 G_DEFINE_TYPE(MarbleSolitaireAppWindow, marble_solitaire_app_window, GTK_TYPE_APPLICATION_WINDOW)
 
+static void new_game(MarbleSolitaireAppWindow *self) {
+  if (self->marble_grid != NULL) {
+    gtk_widget_destroy(self->marble_grid);
+  }
+  self->marble_grid = marble_grid_new();
+  gtk_grid_attach(GTK_GRID(self->grid), self->marble_grid, 0, 1, 3, 1);
+}
+
+static void new_game_button_clicked(GtkButton *button, gpointer data) {
+  MarbleSolitaireAppWindow *self = MARBLE_SOLITAIRE_APP_WINDOW(data);
+  new_game(self);
+  gtk_widget_show_all(GTK_WIDGET(self->grid));
+}
+
 static void marble_solitaire_app_window_init(MarbleSolitaireAppWindow *self) {
   gtk_window_set_title(GTK_WINDOW(self), "Marble Solitaire");
   gtk_container_set_border_width(GTK_CONTAINER(self), 8);
 
-  GtkWidget *marble_grid = marble_grid_new();
-  gtk_container_add(GTK_CONTAINER(self), marble_grid);
-  gtk_widget_show_all(GTK_WIDGET(marble_grid));
+  self->grid = gtk_grid_new();
+  gtk_grid_set_row_spacing(GTK_GRID(self->grid), 15);
+  gtk_container_add(GTK_CONTAINER(self), self->grid);
+
+
+  GtkWidget *new_game_button = gtk_button_new_with_label("New Game");
+  g_signal_connect(new_game_button, "clicked", G_CALLBACK(new_game_button_clicked), self);
+  gtk_grid_attach(GTK_GRID(self->grid), new_game_button, 1, 0, 1, 1);
+
+  self->marble_grid = NULL;
+
+  new_game(self);
+
+  gtk_widget_show_all(GTK_WIDGET(self->grid));
 }
 
 static void marble_solitaire_app_window_class_init(MarbleSolitaireAppWindowClass *klass) {
